d6a18d4709bdbd5ea6cf377f7b97668bbaa3568b
BiuBiuBiuBan <77628259+BiuBiuBiuBan@users.noreply.github.com>
2021-01-29 08:32:05 +0800

    Add files via upload

--- BossRun.cs
+++ BossRun.cs
@@ -15,6 +15,7 @@ public enum BossType
 public class BossRun : MonoBehaviour
 {
     private GameObject player;
+    private FSMSystem fsm;
     private void Awake()
     {
         player = GameObject.Find("player");
@@ -46,44 +47,51 @@ public class BossRun : MonoBehaviour
         StartCoroutine(BossAttack(0.6f));
         anim = transform.GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
+         fsm = new FSMSystem();
+        FSMState patrolState = new PatrolState(fsm);
+        FSMState chaseState = new ChaseState(fsm);
+        fsm.AddState(patrolState);
+        fsm.AddState(chaseState);
+        patrolState.AddTransition(Transition.FindPlayer, StateID.ChaseState);
+        chaseState.AddTransition(Transition.LosePlayer, StateID.PatrolState);
     }
     IEnumerator BossAttack(float t)
     {
 
-        float time = 0;
-        while (true)
-        {
-            Debug.Log("进行中");
+        //float time = 0;
+        //while (true)
+        //{
+        //    Debug.Log("进行中");
 
-            time += 1;
-            float pos = Vector3.Distance(transform.position, player.transform.position);
-            if (time >= 15)
-            {
-                BossState(BossType.Patro);
-                time = 0;
+        //    time += 1;
+        //    float pos = Vector3.Distance(transform.position, player.transform.position);
+        //    if (time >= 15)
+        //    {
+        //        BossState(BossType.Patro);
+        //        time = 0;
 
-            }
-            else if (pos <= shiye)
-            {
-                time = 0;
-                BossState(BossType.Chase);
-                if (pos <= AttackDis)
-                {
-                    if (EXP != 200)
-                    {
-                        BossState(BossType.AttackLevel1);
-                    }
-                    else
-                    {
-                        BossState(BossType.AttackLevel2);
-                    }
+        //    }
+        //    else if (pos <= shiye)
+        //    {
+        //        time = 0;
+        //        BossState(BossType.Chase);
+        //        if (pos <= AttackDis)
+        //        {
+        //            if (EXP != 200)
+        //            {
+        //                BossState(BossType.AttackLevel1);
+        //            }
+        //            else
+        //            {
+        //                BossState(BossType.AttackLevel2);
+        //            }
 
-                }
+        //        }
 
-            }
+        //    }
 
-            yield return new WaitForSeconds(t);
-        }
+         yield return new WaitForSeconds(t);
+        //}
     }
     IEnumerator TimeRun(float t)
     {
@@ -101,52 +109,52 @@ public class BossRun : MonoBehaviour
 
 
     }
-    public void BossState(BossType type)
-    {
-        switch (type)
-        {
-            case BossType.Idle:
-                Debug.Log("发呆阶段");
-                anim.SetFloat("Run", -2);
+    //public void BossState(BossType type)
+    //{
+    //    switch (type)
+    //    {
+    //        case BossType.Idle:
+    //            Debug.Log("发呆阶段");
+    //            anim.SetFloat("Run", -2);
 
-                break;
-            case BossType.Patro:
-                Debug.Log("寻路阶段");
-                Vector3 pos = new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10));
-                nav.SetDestination(pos);
-                anim.SetFloat("Run", 2);
-                if (Vector3.Distance(transform.position, pos) <= 0.5f)
-                {
-                    BossState(BossType.Idle);
-                }
-                break;
-            case BossType.Chase:
-                Debug.Log("追人阶段");
-                transform.LookAt(player.transform);
-                anim.SetFloat("Run", 2);
-                nav.SetDestination(player.transform.position);
-                if (Vector3.Distance(transform.position, player.transform.position) > shiye)
-                {
+    //            break;
+    //        case BossType.Patro:
+    //            Debug.Log("寻路阶段");
+    //            Vector3 pos = new Vector3(UnityEngine.Random.Range(-10, 10), 0, UnityEngine.Random.Range(-10, 10));
+    //            nav.SetDestination(pos);
+    //            anim.SetFloat("Run", 2);
+    //            if (Vector3.Distance(transform.position, pos) <= 0.5f)
+    //            {
+    //                BossState(BossType.Idle);
+    //            }
+    //            break;
+    //        case BossType.Chase:
+    //            Debug.Log("追人阶段");
+    //            transform.LookAt(player.transform);
+    //            anim.SetFloat("Run", 2);
+    //            nav.SetDestination(player.transform.position);
+    //            if (Vector3.Distance(transform.position, player.transform.position) > shiye)
+    //            {
 
-                    BossState(BossType.Idle);
+    //                BossState(BossType.Idle);
 
-                }
-                break;
-            case BossType.AttackLevel1:
-                Debug.Log("开始攻击");
-                anim.SetTrigger("RunAttack");
-                GameObject.Find("Plane").GetComponent<GameRun>().HP -= Attack;
-                GameObject.Find("Plane").GetComponent<GameRun>().HPtext.text = GameObject.Find("Plane").GetComponent<GameRun>().HP.ToString();
-                if (GameObject.Find("Plane").GetComponent<GameRun>().HP <= 0)
-                {
-                    Debug.LogError("游戏结束，你挂了");
-                    Time.timeScale = 0;
-                }
-                break;
-            case BossType.AttackLevel2:
-                break;
-            default:
-                break;
-        }
-    }
+    //            }
+    //            break;
+    //        case BossType.AttackLevel1:
+    //            Debug.Log("开始攻击");
+    //            anim.SetTrigger("RunAttack");
+    //            GameObject.Find("Plane").GetComponent<GameRun>().HP -= Attack;
+    //            GameObject.Find("Plane").GetComponent<GameRun>().HPtext.text = GameObject.Find("Plane").GetComponent<GameRun>().HP.ToString();
+    //            if (GameObject.Find("Plane").GetComponent<GameRun>().HP <= 0)
+    //            {
+    //                Debug.LogError("游戏结束，你挂了");
+    //                Time.timeScale = 0;
+    //            }
+    //            break;
+    //        case BossType.AttackLevel2:
+    //            break;
+    //        default:
+    //            break;
+    //    }
+    //}
 }
